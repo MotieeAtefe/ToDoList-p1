80ec66106058460870446702308537647e7cf9c2
MotieeAtefe <motiee1377@gmaile.com>
2024-10-21 09:02:05 +0330

    Add project files.

---
+++ ToDoList/AddToTask.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace ToDoList
+{
+    partial class AddToTask
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dateTimePickerDueDate = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxTaskName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxDuration = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.label1.Font = new System.Drawing.Font("Sitka Small", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "NewTask";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.label2.Font = new System.Drawing.Font("Sitka Small", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "TimeToDone";
+            // 
+            // dateTimePickerDueDate
+            // 
+            this.dateTimePickerDueDate.Location = new System.Drawing.Point(183, 58);
+            this.dateTimePickerDueDate.Name = "dateTimePickerDueDate";
+            this.dateTimePickerDueDate.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePickerDueDate.TabIndex = 5;
+            this.dateTimePickerDueDate.ValueChanged += new System.EventHandler(this.dtpDueDate_ValueChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(17, 149);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 42);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "btnAddTask";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxTaskName
+            // 
+            this.textBoxTaskName.Location = new System.Drawing.Point(183, 9);
+            this.textBoxTaskName.Name = "textBoxTaskName";
+            this.textBoxTaskName.Size = new System.Drawing.Size(200, 22);
+            this.textBoxTaskName.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.label3.Font = new System.Drawing.Font("Sitka Small", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 105);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(126, 25);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "TimeToDone";
+            // 
+            // textBoxDuration
+            // 
+            this.textBoxDuration.Location = new System.Drawing.Point(183, 105);
+            this.textBoxDuration.Name = "textBoxDuration";
+            this.textBoxDuration.Size = new System.Drawing.Size(200, 22);
+            this.textBoxDuration.TabIndex = 9;
+            this.textBoxDuration.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // AddToTask
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(821, 599);
+            this.Controls.Add(this.textBoxDuration);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxTaskName);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dateTimePickerDueDate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "AddToTask";
+            this.Text = "AddToTask";
+            this.Load += new System.EventHandler(this.AddToTask_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dateTimePickerDueDate;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxTaskName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxDuration;
+    }
+}
---
+++ ToDoList/AddToTask.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ToDoList
+{
+    public partial class AddToTask : Form
+    {
+        DataGridView dataGridView1 = new DataGridView();
+        public AddToTask()
+        {
+            InitializeComponent();
+            dataGridView1.Columns.Add("TaskNameColumn", "Task Name");
+            dataGridView1.Columns.Add("DueDateColumn", "Due Date");
+            dataGridView1.Columns.Add("DurationColumn", "Duration");
+            // setting size DataGridView
+            dataGridView1.Size = new Size(400, 200); 
+
+            // setting Location DataGridView
+            dataGridView1.Location = new Point((this.ClientSize.Width - dataGridView1.Width) - 20, (this.ClientSize.Height - dataGridView1.Height) / 2); 
+
+            // setting show DataGridView
+            dataGridView1.Anchor = AnchorStyles.Right | AnchorStyles.Top | AnchorStyles.Bottom; 
+
+            // Add DataGridView to form controls
+            this.Controls.Add(dataGridView1);
+        }
+
+        private void txtTaskName_TextChanged(object sender, EventArgs e)
+        {
+            string taskName = textBoxTaskName.Text;
+            
+        }
+
+        private void dtpDueDate_ValueChanged(object sender, EventArgs e)
+        {
+            //Time and date of the task
+            DateTime dueDate = dateTimePickerDueDate.Value;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            //Task time
+            string duration = textBoxDuration.Text;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Getting values ​​from the user
+            string taskName = textBoxTaskName.Text;
+            DateTime dueDate = dateTimePickerDueDate.Value;
+            string duration = textBoxDuration.Text;
+
+            // Add a new row here
+            int rowIndex = dataGridView1.Rows.Add();
+            DataGridViewRow row = dataGridView1.Rows[rowIndex];
+
+            row.Cells["TaskNameColumn"].Value = taskName;
+            row.Cells["DueDateColumn"].Value = dueDate;
+            row.Cells["DurationColumn"].Value = duration;
+
+            // cleaning values ​​from the user
+            textBoxTaskName.Clear();
+            textBoxDuration.Clear();
+            dateTimePickerDueDate.Value = DateTime.Now;
+        }
+
+        private void AddToTask_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
